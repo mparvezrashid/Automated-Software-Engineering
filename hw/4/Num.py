90f90070ae597ac62d284aaf04086d0a02fb4371
M PARVEZ RASHID <34420722+mparvezrashid@users.noreply.github.com>
2019-11-01 12:51:49 -0600

    Create Num.py

---
+++ hw/4/Num.py
@@ -0,0 +1,64 @@
+import math
+class Num():
+    "Track numbers seen in a column"
+
+    def __init__(self, inits=[]):
+        self.n, self.mu, self.m2 = 0, 0, 0
+        self.lo, self.hi = 10 ** 32, -1 * 10 ** 32
+        self.sd=0
+        [self + x for x in inits]
+
+    def delta(self):
+        return self.nsd()
+
+    def expect(self):
+        return self.mu
+
+    def variety(i):
+        return i.nsd()
+
+    def nsd(self):
+        #if self.m2 < 0:
+        #   return 0
+        if self.m2 < 0:
+            return 0
+        if self.n < 2:
+            return 0
+        return math.sqrt(self.m2 / (self.n - 1))
+
+    #return 0 if self.n < 2 else (self.m2 / (self.n - 1 + 10 ** -32)) ** 0.5
+
+    def __add__(self, x):
+        if x < self.lo:
+            self.lo = x
+        if x > self.hi:
+            self.hi = x
+
+        self.n += 1
+        d = x - self.mu
+        self.mu += d / self.n
+        self.m2 += d * (x - self.mu)
+        self.sd=self.nsd()
+        return x
+
+    def __sub__(self, x):
+        if self.n < 2:
+            self.n, self.mu, self.m2 = 0, 0, 0
+        else:
+            self.n -= 1
+            d = x - self.mu
+            self.mu -= d / self.n
+            self.m2 -= d * (x - self.mu)
+            self.sd = self.nsd()
+            return x
+
+    def NumLike(self, x):
+        #print("SD" + str(self.sd()) + "x" + str(x))
+        var = self.sd ** 2
+        denom = (3.14159 * 2 * var) ** .5
+        num = 2.71828 ** (-(x - self.mu) ** 2 / (2 * var + 0.0001))
+        return num/(denom+10**(-64)) + 10**(-64)
+
+    def xpect(i, j):
+        n = i.n + j.n
+        return i.n / n * i.variety() + j.n / n * j.variety()
